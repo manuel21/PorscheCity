8b3f7d6465fc02e448da8c2b2b42cf80d4c208ef
Gerardo <>
2017-11-01 19:52:58 -0600

    added functionality for watch notifications

---
+++ Porsche City/Porsche City/Enums/Enums.swift
@@ -0,0 +1,17 @@
+//
+//  Enums.swift
+//  Porsche City
+//
+//  Created by Gerardo on 01/11/17.
+//  Copyright © 2017 mxnuel. All rights reserved.
+//
+
+import Foundation
+
+public enum NotificationType: Int {
+    case restaurantHost = 0
+    case restaurantValet = 1
+    case hotelCheckIn = 2
+    case shuttleDriver = 3
+    case hotelValet = 4
+}
