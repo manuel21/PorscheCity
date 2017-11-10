9021bd33981d2d666d34f8e258b68a283f374e93
Manuel Salinas <manuel.salinas@definityfirst.com>
2017-11-09 20:15:23 -0600

    Checkout animation card

--- Porsche City/Porsche City/Modals/CheckoutController.swift
+++ Porsche City/Porsche City/Modals/CheckoutController.swift
@@ -11,19 +11,30 @@ import UIKit
 class CheckoutController: UIViewController
 {
     //MARK: PROPERTIES & OUTLETS
+    @IBOutlet weak fileprivate var ivCheckout: UIImageView!
+    @IBOutlet weak fileprivate var ivCheckoutSuccess: UIImageView!
+    @IBOutlet weak fileprivate var ivSlideText: UIImageView!
+    @IBOutlet weak fileprivate var ivCheckoutCard: UIImageView!
+    @IBOutlet weak fileprivate var btnNotification: UIButton!
+    @IBOutlet weak fileprivate var contraintCard: NSLayoutConstraint!
+    
     var onDeinit:(() -> ())?
+    fileprivate var wasSlide = false
+    let screenHeight = UIScreen.main.bounds.size.height
+    var finalFrame: CGRect!
     
     //MARK: LIFE CYCLE
     override func viewDidLoad()
     {
         super.viewDidLoad()
-        self.loadConfig()
     }
     
     override func viewWillAppear(_ animated: Bool)
     {
         super.viewWillAppear(animated)
         UIApplication.shared.isStatusBarHidden = true
+        
+        self.ivCheckoutSuccess.alpha = 0
     }
     
     override func viewWillDisappear(_ animated: Bool)
@@ -43,12 +54,7 @@ class CheckoutController: UIViewController
         print("Deinit: CheckoutController")
         self.onDeinit?()
     }
-    
-    //MARK: CONFIG
-    fileprivate func loadConfig()
-    {
-    }
-    
+
     //MARK: ACTIONS
     @IBAction func close()
     {
@@ -63,7 +69,52 @@ class CheckoutController: UIViewController
     
     @IBAction func checkout(_ sender: Any)
     {
-        self.dismiss(animated: true, completion: nil)
+        //iphone 8 = 667
+        //iphone 8 plus = 736
+        //Constraint original 118
+        
+        if self.wasSlide == false
+        {
+            self.wasSlide = true
+            
+            //Slide Down
+             UIView.animate(withDuration: 0.5,  animations: {
+                
+                self.contraintCard.constant = 24
+                self.view.layoutIfNeeded()
+                
+            }, completion: { complete in
+                
+                self.ivSlideText.isHidden = true
+                
+                //Change status card
+                UIView.animate(withDuration: 0.3,  animations: {
+
+                    self.ivCheckoutCard.alpha = 0
+
+                }, completion: { complete in
+
+                    self.ivCheckoutCard.image = #imageLiteral(resourceName: "iconCheckoutPassSuccess")
+                    
+                    //Show success checkout
+                    UIView.animate(withDuration: 0.3,  animations: {
+                        
+                        self.ivCheckoutCard.alpha = 1
+                        self.ivCheckout.alpha = 0
+                        
+                    }, completion: { complete in
+                        
+                        UIView.animate(withDuration: 0.2,  animations: {
+                            
+                            self.ivCheckoutSuccess.alpha = 1
+                            self.btnNotification.isEnabled = false
+                        })
+                    })
+                    
+                })
+                
+            })
+        }
     }
     
     // MARK: - Navigation
