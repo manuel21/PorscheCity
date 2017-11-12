54e978c68bc68b4a110ef5c8b8dff870f870a4db
Nestor Javier Hernandez Bautista <nestor.hernandez@definityfirst.com>
2017-11-11 19:11:55 -0600

    change intinerary

--- Porsche City/Porsche City/Helpers/Cells/CollectionCell_Clone.swift
+++ Porsche City/Porsche City/Helpers/Cells/CollectionCell_Clone.swift
@@ -12,7 +12,7 @@ class CollectionCell_Clone: UITableViewCell
 {    
     //MARK: OUTLETS AND VARIABLES
     @IBOutlet weak fileprivate var collection: UICollectionView!
-    fileprivate var items = ["Appetizer on arrival", "Premium Fuel"]
+    fileprivate var items = ["Appetizer on arrival", "Sommelier", "Luggage Delivery", "Spa Package"]
     
     //MARK: LIFE CYCLE
     override func awakeFromNib()
@@ -57,7 +57,22 @@ extension CollectionCell_Clone: UICollectionViewDelegate, UICollectionViewDataSo
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell
     {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "horizontalItem", for: indexPath) as! horizontalItem
-        cell.icon.image = indexPath.item % 2 == 0 ? #imageLiteral(resourceName: "imgItem3") : #imageLiteral(resourceName: "imgItem4")
+        if indexPath.item == 0
+        {
+            cell.icon.image = #imageLiteral(resourceName: "imgItem3")
+        }
+        else if indexPath.item == 1
+        {
+            cell.icon.image = #imageLiteral(resourceName: "sommelier")
+        }
+        else if indexPath.item == 2
+        {
+            cell.icon.image = #imageLiteral(resourceName: "item5")
+        }
+        else
+        {
+            cell.icon.image = #imageLiteral(resourceName: "spaPackage")
+        }
         cell.title.text = self.items[indexPath.item]
         
         return cell
